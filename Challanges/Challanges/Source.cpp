7535722cf8af6ebd09725882d3d479764826843d
pawelsa <pawel.sapek97@gmail.com>
2016-11-27 22:34:07 +0100

    Nic

---
+++ Challanges/Challanges/Source.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+// 1	Prime factorization(or prime decomposition).For a given positive integer, express it as a product of prime numbers.
+
+
+
+
+
+// 2	Dec2Bin.Write a program reading a decimal integer, converting it to binary and writing the result.
+// 3	Write a function reversing the binary representation of an integer.For example, binreverse(200) = 19, because 20010 = 110010002 and 1910 = 100112.
+// 4	Write a function accepting a text string(a pointer to char) and counting words in this text.
+// 5	Write a function computing the day of week for a given date.
+// 6	Write a function computing the number of days between January the 1st, 2000 and the given date.
+// 7	Find such a 4 - digit number n, that the last 4 digits of n*n are equal n.
+// 8	Write a program multiplying two big integers given as text strings.
+// 9	Write a program converting a text into Morse code.Use a dot, dash and a space as output symbols.
+// 10	Write a program converting Morse code into plain text.Use symbols from the previous task.
+// 11	Write a program finding the longest palindrome(like "hannah") in a given text.
+// 12	Write a program converting a given decimal number into a Roman number.
+// 13	Write a program converting a Roman number into a decimal number.
+// 14	Arrange digits 1 to 9 in form of a fraction equal to 1 / 3 (e.g. 6342 / 91785).
+}
+
+
+
